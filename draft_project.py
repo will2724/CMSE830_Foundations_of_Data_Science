c031c085b342dbddc30ec0bbb15d8316014daee1
Sharod Williams <sharodwilliams8@gmail.com>
2023-10-15 02:36:05 -0400

    right track

---
+++ draft_project.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import re
+import seaborn as sns
+import streamlit as st
+import os
+
+#titles
+st.title('Data Scientist Salaries')
+st.header('IDA')
+
+#load dataset
+@st.cache(persist=True)
+def explore_data(dataset):
+    df = pd.read_csv(os.path.join(dataset))
+    return df
+
+
+
+if st.checkbox("Preview Data")
+    data = explore_data()
+    st.table(df.head())
