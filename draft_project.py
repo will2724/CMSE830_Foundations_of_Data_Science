0212ba89ac39bc9e3aa1a32b94c7747766f63227
Sharod Williams <sharodwilliams8@gmail.com>
2023-10-15 03:01:15 -0400

    right track

--- draft_project.py
+++ draft_project.py
@@ -22,21 +22,22 @@ if st.checkbox('Preview Data'):
     data = explore_data(my_dataset)
     st.table(data.head())
 
-preview = st.checkbox("Preview Data")
-if preview:
+if st.checkbox("Preview Data"):
+    data = explore_data(my_dataset)
     st.table(data.head())
 
-df_shape = st.radio('What is the dimension of:', ('Entire Dataset', 'Rows', 'Columns'))
-if data_shape == 'Entire':
-    st.text('Entire Dataset Shown')
-    st.write(data.shape)
-elif data_shape == 'Rows':
-    st.text('Rows Shown')
-    st.write(data.shape[0])
-else:
-    st.text('Columns Shown')
-    st.write(data.shape[1])
+if data_shape = st.radio('What is the dimension of:', ('Entire Dataset', 'Rows', 'Columns')):
+    data = explore_data(my_dataset)
+    if data_shape == 'Entire':
+        st.text('Entire Dataset Shown')
+        st.write(data.shape)
+    elif data_shape == 'Rows':
+        st.text('Rows Shown')
+        st.write(data.shape[0])
+    else:
+        st.text('Columns Shown')
+        st.write(data.shape[1])
 
-nan_in_ds = st.button('Are there any NaNs in dataset?')
-if nan_in_ds:
+if st.button('Are there any NaNs in dataset?'):
+    data = explore_data(my_dataset)
     st.write(data.isna().any())
