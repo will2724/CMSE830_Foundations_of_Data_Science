1e7ad465ee371c007f0699d9a0d3f3c20333e54f
Sharod Williams <sharodwilliams8@gmail.com>
2023-10-15 03:01:54 -0400

    right track

--- draft_project.py
+++ draft_project.py
@@ -26,7 +26,7 @@ if st.checkbox("Preview Data"):
     data = explore_data(my_dataset)
     st.table(data.head())
 
-if data_shape = st.radio('What is the dimension of:', ('Entire Dataset', 'Rows', 'Columns')):
+if st.radio('What is the dimension of:', ('Entire Dataset', 'Rows', 'Columns')):
     data = explore_data(my_dataset)
     if data_shape == 'Entire':
         st.text('Entire Dataset Shown')
