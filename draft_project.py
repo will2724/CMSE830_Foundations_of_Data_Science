f90ef60e669af4c885f4a4466e369f53e3184e6c
Sharod Williams <sharodwilliams8@gmail.com>
2023-10-15 02:57:56 -0400

    right track

--- draft_project.py
+++ draft_project.py
@@ -17,6 +17,26 @@ def explore_data(data):
     return df
 
 
+
 if st.checkbox('Preview Data'):
     data = explore_data(my_dataset)
     st.table(data.head())
+
+preview = st.checkbox("Preview Data")
+if preview:
+    st.table(data.head())
+
+df_shape = st.radio('What is the dimension of:', ('Entire Dataset', 'Rows', 'Columns'))
+if data_shape == 'Entire':
+    st.text('Entire Dataset Shown')
+    st.write(data.shape)
+elif data_shape == 'Rows':
+    st.text('Rows Shown')
+    st.write(data.shape[0])
+else:
+    st.text('Columns Shown')
+    st.write(data.shape[1])
+
+nan_in_ds = st.button('Are there any NaNs in dataset?')
+if nan_in_ds:
+    st.write(data.isna().any())
