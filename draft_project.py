d96f8a375a8f817ff4007d0d7d77eecdda7e2db8
Sharod Williams <sharodwilliams8@gmail.com>
2023-10-16 02:00:17 -0400

    right track

--- draft_project.py
+++ draft_project.py
@@ -4,7 +4,7 @@ import re
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from streamlit_option menu import option menu
+from streamlit_option_menu import option_menu
 import os
 import plotly.express as px
 import plotly.graph_objects as go
