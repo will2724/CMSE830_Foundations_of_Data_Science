6e2b97c8b780ca6697e3f261400ce3b41a0777f1
Sharod Williams <sharodwilliams8@gmail.com>
2023-11-03 22:52:29 -0400

    mid-term update

--- CMSE830_midterm_project.py
+++ CMSE830_midterm_project.py
@@ -97,12 +97,21 @@ with tab1:
     We long gone past the time when we once lived where every action required user input and the view on how math subjects can be applicable outside of what is required while in school.  Thanks to every growing field on Artificial Intelligence (AI) and Machine Learning (ML) and the well renowned [article published in Harvard Business Review](https://hbr.org/2012/10/data-scientist-the-sexiest-job-of-the-21st-century) back in 2012, which called Data Science the sexiest job of the 21st century. As businesses continue to seek ways effectively access trends (from present data, reviewing trends from past data and accurately predicting future trends), satisfy the need of customers while operating in the most efficient way possible.
 
     It is widely known that pursing a career in Data Science can be rewarding in terms of income, while that may be the case as the great saying goes 'More money, more problems'. This purpose of this app will explore through the job listings from a Glassdoor back in 2016 and observe certain trends in salaries, location, satisfaction of a position, the abundance of positions and see how they all relate to one another.""")
+<<<<<<< HEAD
     #col1, col2 = st.columns(2,gap='large')
     #with col1.expander("Mo Money, Mo Problems"):
     #    st.video("https://www.youtube.com/watch?v=NmowYxzKr6o",start_time=0)
     #with col2.expander("Benjamins"):
      #   st.video("https://www.youtube.com/watch?v=n4p9zpEY6l8",start_time=0)
     #st.write('#')
+=======
+    col1, col2 = st.columns(2,gap='large')
+    #with col1.expander("Mo Money, Mo Problems"):
+    #    st.video("https://www.youtube.com/watch?v=NmowYxzKr6o",start_time=0)
+    #with col2.expander("Benjamins"):
+    #    st.video("https://www.youtube.com/watch?v=n4p9zpEY6l8",start_time=0)
+    st.write('#')
+>>>>>>> cd4f843 (mid-term)
 
 with tab2:
     st.title('Dataset Exploratation')
@@ -149,7 +158,6 @@ with tab2:
             Description Length: A count of total number of characters in the job posting
 
             **MLE: Machine Learning Engineer''')
-
 
         small_vis = st.radio('**Quick visuals**', ['Distributions', 'Coding Skillset'], horizontal=True)
         if small_vis == 'Distributions':
